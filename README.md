6c1639a601764eead7eaaa1ce65d047de581941c
Milterdone <alchemistgomunculus@google.com>
2025-02-01 23:48:38 +0500

    - Initial commit. Should I ignore .gitignore?\

---
+++ README.md
@@ -0,0 +1,44 @@
+# Assignment 3. Part 2.
+## About Task
+In previous task I wrote AITU_SE2324_KV contract that is an ERC-20 smart contract.
+It just makes the tokens and mints them to the person, who deploys the contract
+But in this task I created additional token:
+
+- **AITU_SE2324_KV**: A basic ERC20 token.
+- **AITU_SE2324_KV_Upgraded**: Basic ERC20 token that can take in how many tokens contractor wants to mint.
+
+## About Token  
+Standard ERC-20 openzeppelin token implementation
+
+Mints initial tokens to owner
+
+Tracks: transaction sender, receiver, amount, and timestamp
+
+Functions: transfer, transferFrom, allowance
+
+
+## Installation  
+1. Clone the repository:  
+   ```bash
+   git clone https://github.com/milterdole/Assignment3_Pt2.git
+   cd Assignment3_Pt2
+   ```
+2. Install dependencies:  
+   ```bash
+   npm install
+   npm install @openzeppelin/contracts
+   ```
+3. Hardhat installation:
+    ```bash
+    npm install --save-dev hardhat
+4. Launch tests (all):
+    ```bash
+    npx hadhat test
+    ```
+
+## Demo
+![Of course these tests ARE ALL PASSING](screenshots/test.png)
+
+This is what you should get if you intalled everything correctly.
+
+If you want to look at code: check files in /Contracts and /test
---
+++ test/AITU_SE2324_KV.test.js
@@ -0,0 +1,69 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("AITU_SE2324_KV Token", function () {
+  let Token, token, owner, addr1, addr2;
+
+  beforeEach(async function () {
+    Token = await ethers.getContractFactory("AITU_SE2324_KV");
+    [owner, addr1, addr2] = await ethers.getSigners();
+    token = await Token.deploy(2000);
+  });
+
+  it("Should have correct name and symbol", async function () {
+    expect(await token.name()).to.equal("AITU_SE2324_KV");
+    expect(await token.symbol()).to.equal("AITU");
+  });
+
+  it("Should assign initial supply to owner", async function () {
+    const ownerBalance = await token.balanceOf(owner.address);
+    expect(await token.totalSupply()).to.equal(ownerBalance);
+  });
+
+  it("Should transfer tokens correctly", async function () {
+    const amount = 100n;
+    await token.transfer(addr1.address, amount);
+    expect(await token.balanceOf(addr1.address)).to.equal(amount);
+    expect(await token.balanceOf(owner.address)).to.equal(2000n * 10n ** 18n - amount);
+  });
+
+  it("Should update latestTransaction after transfer", async function () {
+    await token.transfer(addr1.address, 50);
+    const sent = await token.getTransactionSender();
+    expect(sent).to.equal(owner.address);
+    const recieved = await token.getTransactionReceiver();
+    expect(recieved).to.equal(addr1.address);
+  });
+
+  /*
+
+  I'm Commenting These two as they are not actually what was asked from task 3
+  I did Allowance and transferFrom because I thought 
+  I had to overwrite these functions for my coin to work.
+
+  it("Should handle transferFrom with allowance", async function () {
+    await token.approve(addr1.address, 200);
+    await token.connect(addr1).transferFrom(owner.address, addr2.address, 150);
+    expect(await token.balanceOf(addr2.address)).to.equal(150);
+  });
+
+  it("Should fail transferFrom without enough allowance", async function () {
+    await token.approve(addr1.address, 50);
+    await expect(token.connect(addr1).transferFrom(owner.address, addr2.address, 100))
+      .to.be.revertedWithCustomError(token, "ERC20InsufficientAllowance");
+  }); */
+
+  it("Should fail transfer if balance is insufficient", async function () {
+    await expect(token.connect(addr1).transfer(addr2.address, 100))
+      .to.be.revertedWithCustomError(token, "ERC20InsufficientBalance");
+  });
+
+  it("Should return latest transaction timestamp", async function () {
+    await token.transfer(addr1.address, 100);
+    const timeString = await token.getLatestTransactionTimestamp();
+    expect(timeString).to.include("seconds ago"); // If it returns something, it returns AT LEAST something
+  });
+});
+
+
+
